bca84587e339c233e33262954e65de91a0a516e1
NestorRdz02 <nestorivan.rodriguezzavala@gmail.com>
2023-10-21 18:59:12 -0600

    changed index.js

--- middleware/index.js
+++ middleware/index.js
@@ -1,3 +1,3 @@
 module.exports = (req, res, next) => {
-    return res.status(200).json({ code: 1, message: "Bienvenido al pokédex" });
+    return res.status(200).json({ code: 1, message: "Human Resources DataBase" });
 }
