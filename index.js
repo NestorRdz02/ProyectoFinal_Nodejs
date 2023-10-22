43d33e65ff2fafac4b0258876f140d6fc879526e
NestorRdz02 <nestorivan.rodriguezzavala@gmail.com>
2023-10-21 18:24:56 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+//dependencies
+const morgan = require('morgan');
+const express = require('express');
+const app = express();
+//routers
+const pokemon = require('./routes/pokemon');
+const user = require('./routes/user');
+//middleware
+const auth = require('./middleware/auth');
+const notFound = require('./middleware/notFound');
+const index = require('./middleware/index');
+const cors = require('./middleware/cors');
+
+app.use(cors);
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extented: true }));
+
+
+app.get("/", index);
+app.use("/user", user);
+app.use(auth);
+app.use("/pokemon", pokemon);
+app.use(notFound);
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running");
+});
