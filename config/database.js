6b1df22e6b419193724d03a3f149244005abefb5
NestorRdz02 <nestorivan.rodriguezzavala@gmail.com>
2023-10-21 18:56:14 -0600

    Changed database.js

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'pokemon'
+    database: 'human_resources'
 });
 
 pool.query = util.promisify(pool.query);
