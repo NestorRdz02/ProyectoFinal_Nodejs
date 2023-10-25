693d152778a24c6afc3be2413e7f823c2271f6f4
Wuotto-D <danielwuotto@gmail.com>
2023-10-25 11:35:22 -0600

    add prueba

--- human_resources/js/human_resources.js
+++ human_resources/js/human_resources.js
@@ -1,33 +1,74 @@
 window.onload = init;
-var headers = {};
+var headers = {'Content-Type': 'application/json'};
 var url = "http://localhost:3000";
 
-function init() {
-    if (localStorage.getItem("token")) {
-        headers = {
+async function addEmployee() {
+    var name = document.getElementById('employeeName').value;
+    var lastName = document.getElementById('employeeLastName').value;
+    var phone = document.getElementById('employeePhone').value;
+    var email = document.getElementById('employeeEmail').value;
+    var address = document.getElementById('employeeAddress').value;
+
+    try {
+        const response = await fetch('/employees', {
+            method: 'post',
+            body: JSON.stringify({
+                employee_name: name,
+                employee_last_name: lastName,
+                employee_phone_number: phone,
+                employee_mail: email,
+                employee_address: address
+           })
+        });
+        const data = await response.json();
+        document.getElementById('message').textContent = data.message;
+    } catch (error) {
+        console.error('Error:', error);
+        document.getElementById('message').textContent = 'Error al añadir empleado.';
+    }
+}
+
+async function updateEmployee() {
+    const id = document.getElementById('employeeId').value;
+    const name = document.getElementById('employeeName').value;
+    const lastName = document.getElementById('employeeLastName').value;
+    const phone = document.getElementById('employeePhone').value;
+    const email = document.getElementById('employeeEmail').value;
+    const address = document.getElementById('employeeAddress').value;
+
+    try {
+        const response = await fetch(`/employees/${id}`, {
+            method: 'PUT',
             headers: {
-                'Authorization': "bearer" + localStorage.getItem("token")
-            }
-        }
-        loadEmployees();
-    }
-    else {
-        window.location.href = "index.html";
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                employee_name: name,
+                employee_last_name: lastName,
+                employee_phone_number: phone,
+                employee_mail: email,
+                employee_address: address
+            })
+        });
+        const data = await response.json();
+        document.getElementById('message').textContent = data.message;
+    } catch (error) {
+        console.error('Error:', error);
+        document.getElementById('message').textContent = 'Error al actualizar empleado.';
     }
 }
 
-function loadEmployees() {
-    axios.get(url + "/employees", headers).then(function (res) {
-        console.log(res);
-        displayEmployees(res.data.message);
-    }).catch(function (err) {
-        console.log(err);
-    })
-}
+async function deleteEmployee() {
+    const id = document.getElementById('employeeId').value;
 
-function displayEmployees(employees) {
-    var body = document.querySelector("body");
-    for (var i = 0; i < employees.length; i++) {
-        body.innerHTML += `<h3>${employees[i].employee_name}</h3>`;
+    try {
+        const response = await fetch(`/employees/${id}`, {
+            method: 'DELETE'
+        });
+        const data = await response.json();
+        document.getElementById('message').textContent = data.message;
+    } catch (error) {
+        console.error('Error:', error);
+        document.getElementById('message').textContent = 'Error al eliminar empleado.';
     }
 }
